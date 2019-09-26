f6fc7d923cee616aab5e27a355baf723717d95ad
Sagar Kumar <48738501+sagar2460@users.noreply.github.com>
2019-09-27 02:08:58 +0530

    initial commit
    Data Structure of LEVEL ORDER TRAVERSAL
    funtions used:-
    -enqueue
    -dequeue
    -is_empty
    -peek
    -__len__
    -size

---
+++ LevelOrder_Traversal.py
@@ -0,0 +1,75 @@
+class Queue(object):
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1].value
+
+    def __len__(self):
+        return self.size()
+
+    def size(self):
+        return len(self.items)
+
+
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def print_tree(self, traversal_type):
+        if traversal_type == "levelorder":
+            return self.levelorder_print(tree.root)
+
+    def levelorder_print(self, start):
+        if start is None:
+            return
+
+        queue = Queue()
+        queue.enqueue(start)
+
+        traversal = ""
+        while len(queue) > 0:
+            traversal += str(queue.peek()) + "-"
+            node = queue.dequeue()
+
+            if node.left:
+                queue.enqueue(node.left)
+            if node.right:
+                queue.enqueue(node.right)
+
+        return traversal
+
+
+#                                    1
+#                                  /   \
+#                                 2     3
+#                                / \    /
+#                               4   5  6
+
+
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+tree.root.right.left = Node(6)
+
+print(tree.print_tree('levelorder'))
